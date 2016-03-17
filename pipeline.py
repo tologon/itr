1e92fd07277c61dd75c3f3ec7f54f62608e62483
Tologon Eshimkanov <teshimkanov@gmail.com>
2016-03-17 10:54:46 -0400

    Add filtering by stroke width variation along w/ some test images.

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
+import math
 
-img = cv2.imread('test_image.png', 0)
+img = cv2.imread('test_image6.png', 0)
 # Pipeline - MSER detection
 mser = cv2.MSER_create()
 
 regions = mser.detectRegions(img, None)
 hulls = [ cv2.convexHull( p.reshape(-1, 1, 2) ) for p in regions ]
 
-# vis = img.copy()
-# cv2.polylines(vis, hulls, 1, (0, 255, 0))
-# cv2.imshow('img', vis)
+vis = img.copy()
+cv2.polylines(vis, hulls, 1, (0, 255, 0))
+cv2.imshow('img', vis)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 # ------------------------------------------------------------------------------
@@ -42,7 +43,7 @@ def extent(convexHull):
     # print "extent: {}".format( float(area) / rectArea )
     return float(area) / rectArea
 
-lowExtentThreshold = 0.2
+lowExtentThreshold = 0.3
 highExtentThreshold = 0.59
 
 print "BEFORE filter w/ extent: {}".format( len(hulls) )
@@ -71,7 +72,32 @@ print "AFTER filter w/ solidity: {}".format( len(hulls) )
 
 vis = img.copy()
 cv2.polylines(vis, hulls, 1, (0, 255, 0))
-cv2.imshow('img', vis)
+cv2.imshow('img2', vis)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# ------------------------------------------------------------------------------
+# Pipeline - non-text regions removed based on stroke width variation
+def strokeWidthVariation(convexHull):
+    convexHull = convexHull.transpose(2,0,1).reshape(2,-1)
+    convexHull = np.uint8(convexHull)
+    edge = cv2.Canny(convexHull, 100, 200)
+    return edge
+
+edges = [strokeWidthVariation(hull) for hull in hulls]
+
+def strokeWidthMetric(stroke):
+    # print "metric: {}".format( np.std(stroke) / np.mean(stroke) )
+    return np.std(stroke) / np.mean(stroke)
+
+strokeWidthThreshold = 0.99
+
+print "BEFORE filter w/ stroke width variation: {}".format( len(hulls) )
+hulls = [hull for hull, edge in zip(hulls, edges) if strokeWidthMetric(edge) > strokeWidthThreshold or math.isnan(strokeWidthMetric(edge)) ]
+print "AFTER filter w/ stroke width variation: {}".format( len(hulls) )
+
+vis = img.copy()
+cv2.polylines(vis, hulls, 1, (0, 255, 0))
+cv2.imshow('img3', vis)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 # ------------------------------------------------------------------------------
